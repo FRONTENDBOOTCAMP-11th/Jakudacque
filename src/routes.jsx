f698b084e24e7ff2e0d92443fe5c39e361c803c6
kangsujung614 <sjkang614@gmail.com>
2024-12-23 16:01:20 +0900

    ✨ feat: routes 초기 설정 #10

---
+++ src/routes.jsx
@@ -0,0 +1,61 @@
+// main
+import Main from "@pages/product/index";
+// product
+import ProductList from "@pages/product/List";
+import Detail from "@pages/product/Detail";
+import Cart from "@pages/product/Cart";
+// user
+import SignIn from "@pages/user/SignIn";
+import SignUp from "@pages/user/SignUp";
+// admin
+import AdminHome from "@pages/admin/index";
+import AdminProductList from "@pages/admin/product/List";
+import Edit from "@pages/admin/product/Edit";
+import New from "@pages/admin/product/New";
+import AdminOrderList from "@pages/admin/order/List";
+
+import Layout from "Layout";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Main /> }, // 메인 홈
+      // product
+      {
+        path: "list",
+        children: [
+          { index: true, element: <ProductList /> }, // 상품 리스트
+          { path: ":id", element: <Detail /> }, // 상품 상세
+        ],
+      },
+      { path: "cart", element: <Cart /> }, // 장바구니
+      // user
+      { path: "signin", element: <SignIn /> }, // 로그인
+      { path: "signup", element: <SignUp /> }, // 회원가입
+      // admin
+      {
+        path: "admin",
+        element: <AdminHome />, // 어드민 홈
+        children: [
+          {
+            path: "products", // 상품 관리
+            children: [
+              { index: true, element: <AdminProductList /> }, // 상품 목록
+              { path: "edit/:id", element: <Edit /> }, // 상품 수정
+              { path: "new", element: <New /> }, // 상품 추가
+            ],
+          },
+          {
+            path: "order", // 주문 관리
+            children: [{ index: true, element: <AdminOrderList /> }], // 주문 목록
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+export default router;
