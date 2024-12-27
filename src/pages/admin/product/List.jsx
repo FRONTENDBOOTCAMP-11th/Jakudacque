b250d205f2539d6d5ee53cdd4a14918080577c6e
kim-young-dong <opa9904@gmail.com>
2024-12-27 13:33:23 +0900

    ✨ feat: 상품 리스트 페이지에 검색 기능 및 삭제 기능 추가

--- src/pages/admin/product/List.jsx
+++ src/pages/admin/product/List.jsx
@@ -1,50 +1,72 @@
-import tw from "tailwind-styled-components";
-import PageNation from "@components/Pagenation.jsx";
+import {
+  TableTitle,
+  StyledTable,
+  StyledThead,
+  StyledTh,
+  StyledTd,
+} from "@components/AdminTable";
+import AdminSearchBar from "@components/\bAdminSearchBar";
+import Pagination from "@components/Pagenation.jsx";
 import useAxiosInstance from "@hooks/useAxiosInstance";
-import { useQuery } from "@tanstack/react-query";
-import { useLocation } from "react-router-dom";
-
-function useQueryStr() {
-  return new URLSearchParams(useLocation().search);
-}
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import useQueryStr from "@hooks/useQueryStr";
+import Spinner from "@components/Spinner";
+import { IoTrashOutline } from "react-icons/io5";
+import { IoOpenOutline } from "react-icons/io5";
 
 export default function List() {
   let page = useQueryStr().get("page") || 1;
+  let keyword = useQueryStr().get("keyword") || "";
+
   const axios = useAxiosInstance();
+  const queryClient = useQueryClient();
 
   const { data } = useQuery({
-    queryKey: ["productList", page],
-    queryFn: () => axios.get("/products", { params: { page } }),
+    queryKey: ["productList", page, keyword],
+    // 로그인 기능 완성 후 /seller/products로 변경
+    queryFn: () => axios.get("/products", { params: { page, keyword } }),
     select: res => res.data,
     staleTime: 1000 * 10,
   });
 
+  const handleDelete = id => {
+    const result = confirm("정말 삭제하시겠습니까?");
+    if (result) {
+      console.log("삭제", id);
+
+      axios.delete(`/seller/products/${id}`).then(() => {
+        queryClient.invalidateQueries("productList");
+      });
+    }
+  };
+
   console.log(data);
 
   if (!data) {
-    return <div>로딩중...</div>;
+    return <Spinner />;
   }
 
   return (
     <>
       <main className="p-10 min-w-80">
-        <h1>상품 리스트</h1>
-        <div className="flex justify-between">
-          <div>Search Bar</div>
-          <button>Create</button>
-        </div>
-        <div>
+        <TableTitle>상품 리스트</TableTitle>
+
+        <AdminSearchBar>
+          <button>상품 등록</button>
+        </AdminSearchBar>
+
+        <>
           <StyledTable>
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <StyledThead>
               <tr>
                 <StyledTh>상품명</StyledTh>
                 <StyledTh>상품 이미지</StyledTh>
                 <StyledTh>가격</StyledTh>
                 <StyledTh>재고</StyledTh>
                 <StyledTh>카테고리</StyledTh>
-                <StyledTh></StyledTh>
+                <StyledTh className="w-16"></StyledTh>
               </tr>
-            </thead>
+            </StyledThead>
             <tbody>
               {data.item.map(item => (
                 <tr key={item._id}>
@@ -62,35 +84,26 @@ export default function List() {
                     {item.extra.category && item.extra.category.join(" / ")}
                   </StyledTd>
                   <StyledTd>
-                    <button>수정</button>
-                    <button>삭제</button>
+                    <div className="flex justify-end gap-2">
+                      <button>
+                        <IoOpenOutline size={24} className="" />
+                      </button>
+                      <button onClick={() => handleDelete(item._id)}>
+                        <IoTrashOutline size={24} className="text-error" />
+                      </button>
+                    </div>
                   </StyledTd>
                 </tr>
               ))}
             </tbody>
           </StyledTable>
-        </div>
-
-        <PageNation
-          maxPage={data?.pagination.totalPages}
+        </>
+        <Pagination
+          // maxPage={data?.pagination.totalPages}
+          maxPage={10}
           currentPage={Number(page)}
         />
       </main>
     </>
   );
 }
-
-const StyledTable = tw.table`
-  w-full
-  table-auto
-`;
-const StyledTh = tw.th`
-  p-4
-  text-xs text-start
-  border-b  
-`;
-const StyledTd = tw.td`
-  p-2
-  text-xs text-start
-  border-b  
-`;
