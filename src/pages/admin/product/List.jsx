00da53d2ebd776a6be85b639a11eace9cb4933d5
kim-young-dong <opa9904@gmail.com>
2024-12-30 10:28:03 +0900

    ✨ feat: 관리자 페이지에선 헤더,카테고리,푸터가 보이지 않도록 수정. 관리자 페이지 상품 리스트 html 구조 변경

--- src/pages/admin/product/List.jsx
+++ src/pages/admin/product/List.jsx
@@ -27,7 +27,8 @@ export default function List() {
   const { data } = useQuery({
     queryKey: ["productList", page, keyword],
     // 로그인 기능 완성 후 /seller/products로 변경
-    queryFn: () => axios.get("/products", { params: { page, keyword } }),
+    queryFn: () =>
+      axios.get("/products", { params: { page, keyword, limit: 15 } }),
     select: res => res.data,
     staleTime: 1000 * 10,
   });
@@ -55,62 +56,59 @@ export default function List() {
 
   return (
     <>
-      <main className="p-10 min-w-80">
-        <TableTitle>상품 리스트</TableTitle>
+      <TableTitle>상품 리스트</TableTitle>
 
-        <AdminSearchBar>
-          <LinkButton to={`${location.pathname}/new`}>상품 등록</LinkButton>
-        </AdminSearchBar>
+      <AdminSearchBar>
+        <LinkButton to={`${location.pathname}/new`}>상품 등록</LinkButton>
+      </AdminSearchBar>
 
-        <>
-          <StyledTable>
-            <StyledThead>
-              <tr>
-                <StyledTh>상품명</StyledTh>
-                <StyledTh>상품 이미지</StyledTh>
-                <StyledTh>가격</StyledTh>
-                <StyledTh>재고</StyledTh>
-                <StyledTh>카테고리</StyledTh>
-                <StyledTh className="w-16"></StyledTh>
+      <>
+        <StyledTable>
+          <StyledThead>
+            <tr>
+              <StyledTh>상품명</StyledTh>
+              <StyledTh>상품 이미지</StyledTh>
+              <StyledTh>가격</StyledTh>
+              <StyledTh>재고</StyledTh>
+              <StyledTh>카테고리</StyledTh>
+              <StyledTh className="w-16"></StyledTh>
+            </tr>
+          </StyledThead>
+          <tbody>
+            {data.item.map(item => (
+              <tr key={item._id}>
+                <StyledTd>{item.name}</StyledTd>
+                <StyledTd>
+                  <img
+                    src={"https://11.fesp.shop" + item.mainImages[0].path}
+                    alt={item.mainImages[0].name}
+                    className="w-16 h-16"
+                  />
+                </StyledTd>
+                <StyledTd>{item.price}</StyledTd>
+                <StyledTd>{item.quantity}</StyledTd>
+                <StyledTd>
+                  {item.extra.category && item.extra.category.join(" / ")}
+                </StyledTd>
+                <StyledTd>
+                  <div className="flex justify-end gap-2">
+                    <Link to={`${location.pathname}/edit/${item._id}`}>
+                      <IoOpenOutline size={24} className=" hover:text-info" />
+                    </Link>
+                    <button onClick={() => handleDelete(item._id)}>
+                      <IoTrashOutline size={24} className="text-error" />
+                    </button>
+                  </div>
+                </StyledTd>
               </tr>
-            </StyledThead>
-            <tbody>
-              {data.item.map(item => (
-                <tr key={item._id}>
-                  <StyledTd>{item.name}</StyledTd>
-                  <StyledTd>
-                    <img
-                      src={"https://11.fesp.shop" + item.mainImages[0].path}
-                      alt={item.mainImages[0].name}
-                      className="w-16 h-16"
-                    />
-                  </StyledTd>
-                  <StyledTd>{item.price}</StyledTd>
-                  <StyledTd>{item.quantity}</StyledTd>
-                  <StyledTd>
-                    {item.extra.category && item.extra.category.join(" / ")}
-                  </StyledTd>
-                  <StyledTd>
-                    <div className="flex justify-end gap-2">
-                      <Link to={`${location.pathname}/edit/${item._id}`}>
-                        <IoOpenOutline size={24} className=" hover:text-info" />
-                      </Link>
-                      <button onClick={() => handleDelete(item._id)}>
-                        <IoTrashOutline size={24} className="text-error" />
-                      </button>
-                    </div>
-                  </StyledTd>
-                </tr>
-              ))}
-            </tbody>
-          </StyledTable>
-        </>
-        <Pagination
-          // maxPage={data?.pagination.totalPages}
-          maxPage={10}
-          currentPage={Number(page)}
-        />
-      </main>
+            ))}
+          </tbody>
+        </StyledTable>
+      </>
+      <Pagination
+        maxPage={data?.pagination?.totalPages}
+        currentPage={Number(page)}
+      />
     </>
   );
 }
