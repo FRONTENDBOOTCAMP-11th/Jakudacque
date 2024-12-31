50594c6abd2a1a11b3754c6eb170735be06cd46b
kim-young-dong <opa9904@gmail.com>
2024-12-31 11:28:29 +0900

    ✨feat: 관리자 페이지 상수 파일 추가

---
+++ src/constants/admin.js
@@ -0,0 +1,11 @@
+export const PRODUCT_KEYS = {
+  name: "상품명",
+  price: "가격",
+  quantity: "재고",
+  content: "상품 설명",
+  shippingFees: "배송비",
+  mainImages: "상품 이미지",
+  category: "카테고리",
+  isNew: "신상품 여부",
+  isBest: "베스트 상품 여부",
+};
