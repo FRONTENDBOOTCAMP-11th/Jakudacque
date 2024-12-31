b4c109cb7052aee54743ac584ce62741d0e397e6
kangsujung614 <sjkang614@gmail.com>
2024-12-31 09:37:44 +0900

    💄 style: 탑 버튼 위치 지정

--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -2,7 +2,7 @@ import { IoChevronUp } from "react-icons/io5";
 
 export default function TopButton() {
   return (
-    <div className="border-2 border-[#ccc] rounded-full w-12 h-12 flex flex-col items-center">
+    <div className="border-2 border-[#ccc] rounded-full w-12 h-12 flex flex-col items-center fixed right-[4%] bottom-[5.5%] bg-white/90">
       <IoChevronUp color="#444" size="14px" className="mt-1.5" />
       <span className="text-sm leading-4 text-[#444]">TOP</span>
     </div>
