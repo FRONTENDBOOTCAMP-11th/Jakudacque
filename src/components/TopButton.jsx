c348be230cc59dc1e97d7198c87821ac651d3301
kangsujung614 <sjkang614@gmail.com>
2025-01-03 11:09:09 +0900

    ✨ feat: 탑 버튼 기능 추가 #26

--- src/components/TopButton.jsx
+++ src/components/TopButton.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import { IoChevronUp } from "react-icons/io5";
 
 export default function TopButton() {
+  const [showButton, setShowButton] = useState(false);
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 800) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="border-2 border-[#ccc] rounded-full w-12 h-12 flex flex-col items-center fixed right-[4%] bottom-[5.5%] bg-white/90">
-      <IoChevronUp color="#444" size="14px" className="mt-1.5" />
-      <span className="text-sm leading-4 text-[#444]">TOP</span>
-    </div>
+    <>
+      {showButton && (
+        <button
+          className="border-2 border-[#ccc] rounded-full w-12 h-12 flex flex-col items-center fixed right-[4%] bottom-[5.5%] bg-white/90"
+          onClick={scrollToTop}
+        >
+          <IoChevronUp color="#444" size="14px" className="mt-1.5" />
+          <span className="text-sm leading-4 text-[#444]">TOP</span>
+        </button>
+      )}
+    </>
   );
 }
