aba8a833db42e0f0db47230ce9e86f618cae5497
kim-young-dong <opa9904@gmail.com>
2025-01-03 11:15:41 +0900

    ✨ feat: Button 컴포넌트 추가 및 스타일링 기능 구현

---
+++ src/components/Button.jsx
@@ -0,0 +1,32 @@
+import tw from "tailwind-styled-components";
+import PropTypes from "prop-types";
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default function Button({ children, ...rest }) {
+  return (
+    <StyledButton rest={rest} onClick={rest.onClick}>
+      {children}
+    </StyledButton>
+  );
+}
+
+const StyledButton = tw.button`
+  ${({ rest }) => {
+    let className = "";
+
+    if (rest.color) {
+      className += `bg-${rest.color} `;
+    }
+    if (rest.text) {
+      className += `text-${rest.text} `;
+    }
+    return className;
+  }}  
+  px-4 py-2 rounded flex-shrink-0
+`;
