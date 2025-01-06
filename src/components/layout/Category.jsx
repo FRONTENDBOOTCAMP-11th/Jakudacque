0b04355499d0cc355db2d09052ca6895d416cc01
chayoutae <cdx8722@gmail.com>
2025-01-06 17:05:13 +0900

    ✨ 카테고리 정보 가져오기, 필터링,  페이지 이동 기능구현

--- src/components/layout/Category.jsx
+++ src/components/layout/Category.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Category = () => {
-  const [activeItem, setActiveItem] = useState(null);
-
+  const navigate = useNavigate();
   const menuItems = [
-    "전체상품",
-    "신상",
-    "BEST",
-    "다이어리",
-    "스티커",
-    "메모지",
-    "마스킹 테이프",
-    "키링",
-    "고객센터",
+    { name: "전체상품", code: "ALL" },
+    { name: "신상" },
+    { name: "BEST" },
+    { name: "다이어리", code: "PC01" },
+    { name: "스티커", code: "PC02" },
+    { name: "메모지", code: "PC03" },
+    { name: "마스킹 테이프", code: "PC04" },
+    { name: "키링", code: "PC05" }
   ];
+
+  const [activeItem, setActiveItem] = useState("전체상품");
+
+  const handleCategoryClick = (item) => {
+    if (!item.code) return;
+    
+    setActiveItem(item.name);
+    if (item.code === "ALL") {
+      navigate("/list?page=1");
+    } else {
+      navigate(`/list?category=${item.code}&page=1`);
+    }
+  };
 
   return (
     <nav className="w-full bg-white border-b border-gray-200 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overflow-x-auto">
@@ -22,17 +34,17 @@ const Category = () => {
           <div className="flex gap-12 md:gap-8 sm:gap-2 whitespace-nowrap">
             {menuItems.map(item => (
               <button
-                key={item}
-                className={`relative px-3 py-2 md:py-1.5 text-sm md:text-[15px] sm:text-xs font-medium 
-                                transition-colors duration-200 border-0 bg-transparent cursor-pointer shrink-0
-                                ${
-                                  activeItem === item
-                                    ? 'text-yellow-300 after:content-[""] after:absolute after:bottom-[-1px] after:left-0 after:w-full after:h-0.5 after:bg-yellow-300'
-                                    : "hover:text-yellow-300"
-                                }`}
-                onClick={() => setActiveItem(item)}
+                key={item.name}
+                className={`relative px-3 py-2 md:py-1.5 text-sm md:text-[15px] sm:text-xs font-medium
+                          transition-colors duration-200 border-0 bg-transparent cursor-pointer shrink-0
+                          ${
+                            activeItem === item.name
+                              ? 'text-yellow-300 after:content-[""] after:absolute after:bottom-[-1px] after:left-0 after:w-full after:h-0.5 after:bg-yellow-300'
+                              : "hover:text-yellow-300"
+                          }`}
+                onClick={() => handleCategoryClick(item)}
               >
-                {item}
+                {item.name}
               </button>
             ))}
           </div>
