ba621a526e36b220c01f2772ca493b47a4d91899
kim-young-dong <opa9904@gmail.com>
2025-01-06 11:23:03 +0900

    ✨ feat : Toast 컴포넌트 스타일 수정 및 closeButton 활성화

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,29 +1,26 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import tw from "tailwind-styled-components";
 
 export default function ToastProvider() {
   return (
-    <StyledToastContainer
-      position="top-center"
+    <ToastContainer
+      position={"top-center"}
       autoClose={3000}
       hideProgressBar
       newestOnTop
-      closeButton={false}
+      closeButton={true}
       toastClassName={() =>
         "bg-[white] text-[#333] p-5 border-2 rounded-lg border-warning shadow-lg text-center"
       }
       bodyClassName={() => "text-sm whitespace-normal"}
+      style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "auto",
+        zIndex: 9999,
+      }}
     />
   );
 }
-
-const StyledToastContainer = tw(ToastContainer)`
-  flex 
-  items-center 
-  justify-center 
-  fixed 
-  inset-0 
-  z-50 
-  pointer-events-none
-`;
