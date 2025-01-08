1bbdee9ee7ed79e13bf54058a1b1410cc4c44106
kim-young-dong <opa9904@gmail.com>
2025-01-08 09:15:49 +0900

    ✨ feat: 관리 대시보드에 날짜 선택 기능 추가

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,9 +1,42 @@
 import { TableTitle } from "@components/AdminTable";
+import { useEffect, useMemo, useState } from "react";
 
 export default function AdminHome() {
+  const [period, setPeriod] = useState({
+    startDate: "",
+    endDate: "",
+  });
+
+  useEffect(() => {
+    setPeriod({
+      startDate: new Date().toISOString().slice(0, 10),
+      endDate: new Date().toISOString().slice(0, 10),
+    });
+  }, []);
+
   return (
     <>
       <TableTitle>대시보드</TableTitle>
+      {/* date-input */}
+      <div className="flex items-center gap-4 mt-4">
+        <input
+          type="date"
+          name="시작일"
+          id=""
+          className="p-2 border rounded"
+          value={period.startDate}
+          onChange={e => setPeriod({ ...period, startDate: e.target.value })}
+        />
+        <span>~</span>
+        <input
+          type="date"
+          name="종료일"
+          id=""
+          className="p-2 border rounded"
+          value={period.endDate}
+          onChange={e => setPeriod({ ...period, endDate: e.target.value })}
+        />
+      </div>
     </>
   );
 }
