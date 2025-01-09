ba73d003ea413c145ad11b9d9eb0d36d1579b1df
kangsujung614 <sjkang614@gmail.com>
2025-01-09 16:11:28 +0900

    ✨ feat: 찜 기능 커스텀 훅으로 추가 #80

---
+++ src/hooks/useHandleWish.js
@@ -0,0 +1,91 @@
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import useWishState from "@zustand/wishState";
+
+export const useHandleWish = _id => {
+  // 찜 상태 변경
+  const toggleWish = useWishState(state => state.toggleWish);
+
+  // 찜 상태 조회
+  const isWished = useWishState(state => state.isWished);
+
+  // 찜 상태 불러오기
+  const restoreWishState = useWishState(state => state.restoreWishState);
+
+  const axios = useAxiosInstance();
+
+  // 상품 찜 한 건 조회
+  const { refetch } = useQuery({
+    queryKey: ["bookmarks", _id],
+    queryFn: async () => {
+      const res = await axios.get(`bookmarks/product/${_id}`);
+      return res.data.item;
+    },
+    enabled: false,
+    onSuccess: data => {
+      console.log("조회 성공:", data);
+    },
+    onError: err => {
+      console.log("에러", err);
+    },
+  });
+
+  // 상품 찜 목록 조회
+  const { refetch: refetchAll } = useQuery({
+    queryKey: ["bookmarks"],
+    queryFn: async () => {
+      const res = await axios.get(`bookmarks/product`);
+      return res.data.item;
+    },
+    enabled: false,
+    onSuccess: data => {
+      console.log("조회 성공:", data);
+    },
+    onError: err => {
+      console.log("에러", err);
+    },
+  });
+
+  // 상품 찜하기
+  const registerWish = useMutation({
+    mutationFn: products => axios.post(`/bookmarks/product/`, products),
+    onSuccess: () => {
+      toggleWish(_id);
+    },
+    onError: err => {
+      console.log(err);
+    },
+  });
+
+  // 상품 찜 취소하기
+  const removeWish = useMutation({
+    mutationFn: _id => axios.delete(`/bookmarks/${Number(_id)}`),
+    onSuccess: () => {
+      toggleWish(_id);
+    },
+    onError: err => {
+      console.log(err);
+    },
+  });
+
+  const refetchCallbacks = async () => {
+    if (isWished(_id)) {
+      const result = await refetch();
+      removeWish.mutate(result.data._id);
+    } else if (!isWished(_id)) {
+      registerWish.mutate({ target_id: Number(_id) });
+    }
+  };
+
+  const refetchAllCallbacks = async () => {
+    const result = await refetchAll();
+    const arr = result.data.map(e => e.product._id);
+    restoreWishState(arr);
+  };
+
+  return {
+    isWished,
+    refetchWish: refetchCallbacks,
+    refetchAll: refetchAllCallbacks,
+  };
+};
