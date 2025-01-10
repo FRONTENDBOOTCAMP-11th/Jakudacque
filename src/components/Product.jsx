a1cbbbf2bb5ae074f46e3ac22f74afbab3c59155
kangsujung614 <sjkang614@gmail.com>
2025-01-10 13:39:33 +0900

    ✨ feat: 마이페이지 찜 내역 조회 기능 추가 #94

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,17 @@
+import { useHandleWish } from "@hooks/useHandleWish";
+import useWishState from "@zustand/wishState";
 import PropTypes from "prop-types";
-import { IoCartOutline, IoHeartOutline } from "react-icons/io5";
+import { IoCartOutline, IoHeartOutline, IoHeartSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 export default function Product({ product }) {
+  const { refetchWish } = useHandleWish(product.id);
+
+  // 찜 상태 조회하는 함수
+  const isWished = useWishState(state => state.isWished);
+
   return (
-    <Link to={product.link} className="relative px-1 mx-auto">
+    <Link to={`/list/${product.id}`} className="relative px-1 mx-auto">
       <img
         src={product.image}
         alt={product.name}
@@ -19,7 +26,13 @@ export default function Product({ product }) {
       </div>
       <div className="flex gap-2 px-1 mb-3">
         <IoCartOutline size={20} />
-        <IoHeartOutline size={20} />
+        <button onClick={refetchWish}>
+          {isWished(product.id) ? (
+            <IoHeartSharp size={20} />
+          ) : (
+            <IoHeartOutline size={20} />
+          )}
+        </button>
       </div>
     </Link>
   );
@@ -27,9 +40,10 @@ export default function Product({ product }) {
 
 Product.propTypes = {
   product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-    link: PropTypes.string.isRequired,
+    // link: PropTypes.string.isRequired,
   }).isRequired,
 };
--- src/pages/user/MyPage.jsx
+++ src/pages/user/MyPage.jsx
@@ -1,16 +1,14 @@
 import Product from "@components/Product";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useQuery } from "@tanstack/react-query";
 import useUserStore from "@zustand/userStore";
 import useWishState from "@zustand/wishState";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function MyPage() {
-  const product = {
-    image: "images/diary1.png",
-    name: "Product 1",
-    price: 1500,
-    link: "https://example.com/page1",
-  };
+  const axios = useAxiosInstance();
 
   const { resetUser } = useUserStore();
   const { resetWishState } = useWishState();
@@ -24,13 +22,37 @@ export default function MyPage() {
     toast("로그아웃 되었습니다!");
   };
 
+  const [wishIsShow, setwishIsShow] = useState(false);
+
+  // 로그인한 회원 데이터
+  const user = JSON.parse(sessionStorage.getItem("user")).state.user;
+  const user_id = user._id;
+
+  // 회원 찜 내역 조회
+  const { data } = useQuery({
+    queryKey: ["user", user_id],
+    queryFn: () => axios.get(`users/${user_id}/bookmarks`),
+    select: res => res.data.item.product,
+  });
+
+  // 찜 상품 데이터 커스텀
+  let product;
+  if (data) {
+    product = data.map(e => ({
+      image: `https://11.fesp.shop/${e.product.mainImages[0].path}`,
+      name: e.product.name,
+      price: e.product.price,
+      id: e.product._id,
+    }));
+  }
+
   return (
     <div className="w-full">
       <div className="max-w-screen-xl mx-auto">
         {/* 프로필 영역 */}
         <div className=" bg-[#f1f1f1] py-12 max-[500px]:py-8 flex items-center gap-x-40 max-[1000px]:gap-x-20 max-[700px]:gap-x-10 justify-center relative max-[500px]:flex-col max-[500px]:gap-y-7">
           <p className="text-[20px] max-[1000px]:text-[18px] max-[700px]:text-[16px] max-[500px]:text-[18px] ml-[-30px] max-[700px]:ml-0 shrink-0 tracking-wide">
-            <span>강수정</span>님, 반갑습니다.
+            <span>{user.name}</span>님, 반갑습니다.
           </p>
           <div className="flex items-center pb-4">
             <div className="text-[16px] max-[700px]:text-[14px] border-x border-[#ccc] px-20 max-[900px]:px-14 max-[700px]:px-10 py-3 shrink-0 max-[500px]:border-x-0 max-[500px]:border-r">
@@ -43,7 +65,7 @@ export default function MyPage() {
             <div className="text-[16px] max-[700px]:text-[14px] border-r border-[#ccc] px-20 max-[900px]:px-14 max-[700px]:px-10 py-3 shrink-0 max-[500px]:border-x-0">
               찜
               <span className="text-[22px] max-[700px]:text-[20px] pl-3 pr-1">
-                2
+                {data?.length}
               </span>
               건
             </div>
@@ -59,12 +81,24 @@ export default function MyPage() {
         {/* 주문내역, 찜 리스트 카테고리 */}
         <div className="flex justify-around gap-x-5 max-[700px]:flex-col">
           <ul className="sticky top-0 ml-5 max-[700px]:ml-0 text-[18px] py-8 max-[700px]:my-6 flex flex-col max-[700px]:flex-row max-[700px]:justify-around gap-y-2 shrink-0 max-[700px]:text-[14px] h-11 max-[700px]:py-0 max-[700px]:gap-y-0 max-[700px]:h-6 bg-[#fff]">
-            <li className="border-b-2 border-[#333]">주문 내역</li>
-            <li>찜 리스트</li>
+            <button
+              className={`${wishIsShow ? "" : "border-b-2 border-[#333]"}`}
+              onClick={() => setwishIsShow(false)}
+            >
+              주문 내역
+            </button>
+            <button
+              className={`${wishIsShow ? "border-b-2 border-[#333]" : ""}`}
+              onClick={() => setwishIsShow(true)}
+            >
+              찜 리스트
+            </button>
           </ul>
 
           {/* 주문 내역 */}
-          <div className="last:border-b-0 basis-[1120px] max-[700px]:basis-0 pt-4 max-[500px]:pt-0">
+          <div
+            className={`last:border-b-0 basis-[1120px] max-[700px]:basis-0 pt-4 max-[500px]:pt-0 ${wishIsShow ? "hidden" : ""}`}
+          >
             <div className="flex flex-col gap-y-4 max-w-[1120px] py-5 border-b border-[#ddd] max-[700px]:px-2">
               <p>25/01/01</p>
               <div className="flex items-center gap-x-5">
@@ -149,15 +183,18 @@ export default function MyPage() {
           </div>
 
           {/* 찜 리스트 */}
-          <div className="max-[600px]:px-2 hidden">
+          <div className={`max-[600px]:px-2 ${wishIsShow ? "" : "hidden"}`}>
             <div className="max-w-[1120px] grid gap-5 pt-10 grid-cols-4 max-[1000px]:grid-cols-3 max-[600px]:grid-cols-2 max-[700px]:pt-0">
-              <Product product={product} />
-              <Product product={product} />
-              <Product product={product} />
-              <Product product={product} />
-              <Product product={product} />
-              <Product product={product} />
-              <Product product={product} />
+              {Array.isArray(product) ? (
+                <>
+                  <Product product={product[0]} />
+                  <Product product={product[1]} />
+                  <Product product={product[2]} />
+                  <Product product={product[3]} />
+                </>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
         </div>
