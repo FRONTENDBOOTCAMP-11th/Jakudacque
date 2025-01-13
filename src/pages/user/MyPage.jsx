a7fae2ecca7d88785abf17fad980717c41749dbd
kangsujung614 <sjkang614@gmail.com>
2025-01-13 14:02:10 +0900

    ✨ feat: 마이페이지 주문 조회 기능 추가 #100

--- src/pages/user/MyPage.jsx
+++ src/pages/user/MyPage.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import CartModal from "@components/CartModal";
+import OrderProduct from "@components/OrderProduct";
 
 export default function MyPage() {
   const axios = useAxiosInstance();
@@ -47,6 +48,23 @@ export default function MyPage() {
     }));
   }
 
+  // 주문 내역 조회
+  const { data: orderData } = useQuery({
+    queryKey: ["order"],
+    queryFn: () => axios.get(`/orders`),
+    select: res => res.data.item,
+  });
+
+  // 주문 내역 데이터 커스텀
+  let orderProducts;
+  if (orderData) {
+    orderProducts = orderData.map(e => ({
+      id: e._id,
+      date: e.createdAt.slice(2, 10).split(".").join("/"),
+      products: e.products,
+    }));
+  }
+
   return (
     <div className="w-full">
       <div className="max-w-screen-xl mx-auto">
@@ -59,7 +77,7 @@ export default function MyPage() {
             <div className="text-[16px] max-[700px]:text-[14px] border-x border-[#ccc] px-20 max-[900px]:px-14 max-[700px]:px-10 py-3 shrink-0 max-[500px]:border-x-0 max-[500px]:border-r">
               주문
               <span className="text-[22px] max-[700px]:text-[20px] pl-3 pr-1">
-                3
+                {orderData?.length}
               </span>
               건
             </div>
@@ -80,7 +98,7 @@ export default function MyPage() {
           </Link>
         </div>
         {/* 주문내역, 찜 리스트 카테고리 */}
-        <div className="flex justify-around gap-x-5 max-[700px]:flex-col">
+        <div className="flex justify-around gap-x-8 max-[700px]:flex-col">
           <ul className="sticky top-0 ml-5 max-[700px]:ml-0 text-[18px] py-8 max-[700px]:my-6 flex flex-col max-[700px]:flex-row max-[700px]:justify-around gap-y-2 shrink-0 max-[700px]:text-[14px] h-11 max-[700px]:py-0 max-[700px]:gap-y-0 max-[700px]:h-6 bg-[#fff]">
             <button
               className={`${wishIsShow ? "" : "border-b-2 border-[#333]"}`}
@@ -100,87 +118,15 @@ export default function MyPage() {
           <div
             className={`last:border-b-0 basis-[1120px] max-[700px]:basis-0 pt-4 max-[500px]:pt-0 ${wishIsShow ? "hidden" : ""}`}
           >
-            <div className="flex flex-col gap-y-4 max-w-[1120px] py-5 border-b border-[#ddd] max-[700px]:px-2">
-              <p>25/01/01</p>
-              <div className="flex items-center gap-x-5">
-                <img
-                  className="w-[100px]"
-                  src="images/sticker1.png"
-                  alt="모서리 스튜디오 햄찌세자매 줄줄이 스티커"
-                />
-                <ul>
-                  <li className="text-[16px]">
-                    모서리 스튜디오 햄찌세자매 줄줄이 스티커
-                  </li>
-                  <li className="text-[16px]">1개</li>
-                  <li className="text-[20px]">2,800원</li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-y-4 max-w-[1120px] py-5 border-b border-[#ddd] max-[700px]:px-2">
-              <p>24/12/31</p>
-              <div className="flex items-center gap-x-5">
-                <img
-                  className="w-[100px]"
-                  src="images/sticker1.png"
-                  alt="모서리 스튜디오 햄찌세자매 줄줄이 스티커"
-                />
-                <ul>
-                  <li className="text-[16px]">
-                    모서리 스튜디오 햄찌세자매 줄줄이 스티커
-                  </li>
-                  <li className="text-[16px]">1개</li>
-                  <li className="text-[20px]">2,800원</li>
-                </ul>
-              </div>
-              <div className="flex items-center gap-x-5">
-                <img
-                  className="w-[100px]"
-                  src="images/sticker1.png"
-                  alt="모서리 스튜디오 햄찌세자매 줄줄이 스티커"
-                />
-                <ul>
-                  <li className="text-[16px]">
-                    모서리 스튜디오 햄찌세자매 줄줄이 스티커
-                  </li>
-                  <li className="text-[16px]">1개</li>
-                  <li className="text-[20px]">2,800원</li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-y-4 max-w-[1120px] py-5 border-b border-[#ddd] max-[700px]:px-2">
-              <p>24/12/31</p>
-              <div className="flex items-center gap-x-5">
-                <img
-                  className="w-[100px]"
-                  src="images/sticker1.png"
-                  alt="모서리 스튜디오 햄찌세자매 줄줄이 스티커"
-                />
-                <ul>
-                  <li className="text-[16px]">
-                    모서리 스튜디오 햄찌세자매 줄줄이 스티커
-                  </li>
-                  <li className="text-[16px]">1개</li>
-                  <li className="text-[20px]">2,800원</li>
-                </ul>
-              </div>
-              <div className="flex items-center gap-x-5">
-                <img
-                  className="w-[100px]"
-                  src="images/sticker1.png"
-                  alt="모서리 스튜디오 햄찌세자매 줄줄이 스티커"
-                />
-                <ul>
-                  <li className="text-[16px]">
-                    모서리 스튜디오 햄찌세자매 줄줄이 스티커
-                  </li>
-                  <li className="text-[16px]">1개</li>
-                  <li className="text-[20px]">2,800원</li>
-                </ul>
-              </div>
-            </div>
+            {orderProducts ? (
+              <>
+                {orderProducts.map(e => (
+                  <OrderProduct key={e.id} orderProducts={e} />
+                ))}
+              </>
+            ) : (
+              <></>
+            )}
           </div>
 
           {/* 찜 리스트 */}
