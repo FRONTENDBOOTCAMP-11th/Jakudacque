8f1fb09be31c90a6c4b6987eccfa4eecf29c80a1
kim-young-dong <opa9904@gmail.com>
2025-01-14 13:27:01 +0900

    ✨ feat: 대시보드에 주문 통계 라인 차트 추가 및 기간 설정 기능 개선

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,5 +1,41 @@
 import { TableTitle } from "@components/AdminTable";
 import { useEffect, useMemo, useState } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+const data = [
+  { name: "2025.01.04", order: 1000 },
+  { name: "2025.01.05", order: 300 },
+  { name: "2025.01.06", order: 200 },
+  { name: "2025.01.07", order: 278 },
+  { name: "2025.01.08", order: 189 },
+  { name: "2025.01.09", order: 239 },
+  { name: "2025.01.10", order: 349 },
+];
+
+const renderLineChart = () => {
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <LineChart
+        data={data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        style={{ width: "100%" }}
+      >
+        <Line type="monotone" dataKey="order" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default function AdminHome() {
   const [period, setPeriod] = useState({
@@ -7,10 +43,26 @@ export default function AdminHome() {
     endDate: "",
   });
 
+  useEffect(() => {}, [period]);
+
   useEffect(() => {
+    const today = new Date(); // 현재 날짜
+    const oneWeekAgo = new Date(); // 현재 날짜를 복사
+
+    oneWeekAgo.setDate(today.getDate() - 7); // 일주일 전으로 설정
+
+    // yyyy-mm-dd 형식으로 변환
+    const formatDate = date => {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1
+      const day = String(date.getDate()).padStart(2, "0");
+      return `${year}-${month}-${day}`;
+    };
+
+    // 기본 기간 7일로 설정 yyyy-mm-dd
     setPeriod({
-      startDate: new Date().toISOString().slice(0, 10),
-      endDate: new Date().toISOString().slice(0, 10),
+      startDate: formatDate(oneWeekAgo),
+      endDate: formatDate(today),
     });
   }, []);
 
@@ -37,6 +89,7 @@ export default function AdminHome() {
           onChange={e => setPeriod({ ...period, endDate: e.target.value })}
         />
       </div>
+      <div className="w-full chart">{renderLineChart()}</div>
     </>
   );
 }
