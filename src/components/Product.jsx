f4b8135efef755a75fd76a0fedc6f0384a46637a
chayoutae <cdx8722@gmail.com>
2025-01-20 17:18:21 +0900

    🐛 propTypes에 id의 타입을 number -> string으로 변경경

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -65,7 +65,7 @@ export default function Product({ product }) {
 
 Product.propTypes = {
   product: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
