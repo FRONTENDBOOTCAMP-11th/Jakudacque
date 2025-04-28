babcf738fd7bef6657e47d3e1671a744870731ba
kangsujung614 <sjkang614@gmail.com>
2025-04-28 13:50:16 +0900

    ✨ feat: 마이페이지 - 주문내역에 리뷰 작성 버튼 추가 #217

--- src/components/OrderDetail.jsx
+++ src/components/OrderDetail.jsx
@@ -5,17 +5,20 @@ export default function OrderDetail({ product }) {
     <>
       <div className="flex items-center gap-x-5">
         <img
-          className="w-24 rounded-md "
+          className="w-24 rounded-md"
           src={`https://11.fesp.shop/${product.image.path}`}
           alt={product.name}
         />
-        <ul className="">
+        <ul>
           <li>{product.name}</li>
           <li>{product.quantity}개</li>
           <li className="pt-5 font-medium">
             {product.price.toLocaleString()}원
           </li>
         </ul>
+        <button className="ml-auto mt-auto border px-2 py-1.5 rounded text-sm border-neutral-300 hover:border-neutral-400">
+          리뷰 작성
+        </button>
       </div>
     </>
   );
